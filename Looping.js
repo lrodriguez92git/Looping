fbe03e8e7b216808dba66ee65f0b9137431c9d31
lrodriguez92git <77756386+lrodriguez92git@users.noreply.github.com>
2021-05-02 19:47:32 -0400

    Add files via upload

---
+++ Looping.js
@@ -0,0 +1,14 @@
+for (var x=1000; x>0; x--){
+console.log(x)
+
+}
+for (var y=0; y<10000; y++){
+
+if (y==2500) {alert ("A quarter of the way there") }
+if (y==5000) {alert ("Halfway there!")}
+if (y==10000) {alert ("The loop is done")}
+
+}
+
+var a= ["Breaking Bad", "Baking", "Friends", "The Chosen", "Cooking"]
+for  (var b=0; b<5; b++) {console.log("My #",b, "favorite tv show is", a[b])}
